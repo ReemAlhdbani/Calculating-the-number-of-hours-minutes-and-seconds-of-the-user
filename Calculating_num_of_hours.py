fc29bcf9b9871abb0e6723d3206bc8b012baf611
ReemAlhdbani <148048593+ReemAlhdbani@users.noreply.github.com>
2024-08-21 13:24:16 +0300

    Add files via upload

---
+++ Calculating_num_of_hours.py
@@ -0,0 +1,8 @@
+
+
+seconds = int(input (" Enter number of seconds : "))
+hours = seconds // 3600
+minuts = (seconds % 3600) // 60
+reming_second = seconds % 60
+print(" here is number of  hours {} \n number of  minuts {} \n reming second {} ".format(hours,minuts,reming_second))
+
